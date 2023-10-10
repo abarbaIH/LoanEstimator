88931f98dc0b346ff5d7a2fef94765bc0490bce9
Alvaro Barba <abarba.lod@gmail.com>
2023-10-10 20:45:49 +0200

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Header from "./components/Header"
+import Button from "./components/Button"
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  }, [cantidad, meses]);
+
+  useEffect(() => {
+    setPago(total / meses);
+  }, [total]);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange(e) {
+    setCantidad(+e.target.value);
+  }
+
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('Cantidad no válida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert('Cantidad no válida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <div className='flex justify-between my-6'>
+        <Button
+          operador='-'
+          fn={handleClickDecremento}
+        />
+        <Button
+          operador='+'
+          fn={handleClickIncremento}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <input
+        type='range'
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+      />
+
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+        {formatearDinero(cantidad)}
       </p>
-    </>
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+        Elige un <span className='text-indigo-600'>Plazo </span> a pagar
+      </h2>
+
+      <select
+        className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+        value={meses}
+        onChange={e => setMeses(+e.target.value)}
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+
+      <div className='my-5 space-y-3 bg-gray-50 p-5'>
+        <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+          Resumen <span className='text-indigo-600'>de pagos </span>
+        </h2>
+
+        <p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
+        <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(total)} Total a pagar</p>
+        <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(pago)} Mensuales</p>
+      </div>
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+        Desarrollado por: <span className='text-indigo-600'>Álvaro Barba </span>
+      </h2>
+      <a className='block my-5' target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/alvaro-barba/">Linkedin</a>
+      <a className='block mb-5' target="_blank" rel="noreferrer noopener" href="https://portfolio-git-main-abarbaih.vercel.app/">Vísita mi Portfolio</a>
+
+
+    </div>
+
+
   )
 }
+
 
 export default App
